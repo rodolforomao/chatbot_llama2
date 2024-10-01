caa2e2bcb86d82e683435463953f7ffcc4f93372
Rodolfo Oliviera <rodolforomao@gmail.com>
2024-09-30 22:39:46 -0300

    atualização

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-version = '1.00.006'
+version = '1.00.007'
 
 app.get('/ok', (req, res) => {
     res.send('Your app is runing. version=' + version);
@@ -26,10 +26,10 @@ app.post('/chat', async (req, res) => {
     try {
         const response = await hf.textGeneration({
             //model: 'meta-llama/Llama-2-7b-chat-hf', // Substitua pelo modelo que você deseja usar
-            //model: 'EleutherAI/gpt-neo-1.3B', // Replace with a free model
-            //model: 'EleutherAI/gpt-j-6B',
-            //model: 'bigscience/bloom-560m', // Replace with a smaller, free model
-            model: 'EleutherAI/gpt-neo-125M', 
+            //model: 'EleutherAI/gpt-neo-1.3B', // muito fraco
+            model: 'EleutherAI/gpt-j-6B',
+            //model: 'bigscience/bloom-560m', 
+            //model: 'EleutherAI/gpt-neo-125M', // Muito fraco 
             inputs: question,
             parameters: {
                 temperature: 0.7, // Optional, you can adjust model parameters as needed
