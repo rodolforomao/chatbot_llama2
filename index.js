013d90a331dea6800988d196d15abe13c75feca8
Rodolfo Oliviera <rodolforomao@gmail.com>
2024-09-30 22:43:10 -0300

    atualização

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-version = '1.00.008'
+version = '1.00.009'
 
 app.get('/ok', (req, res) => {
     res.send('Your app is runing. version=' + version);
