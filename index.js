ce1d8c30b34c6834cffb24c1bebca08dd3efd9b8
Rodolfo Oliviera <rodolforomao@gmail.com>
2024-09-30 22:13:38 -0300

    Correções

--- index.js
+++ index.js
@@ -20,31 +20,19 @@ app.get('/ok', (req, res) => {
 
 
 app.post('/chat', async (req, res) => {
-    const { message } = req.body;
+    const { question } = req.body;
     try {
         const response = await hf.textGeneration({
             model: 'meta-llama/Llama-2-7b-chat-hf', // Substitua pelo modelo que você deseja usar
-            inputs: message,
+            inputs: question,
         });
 
         res.json({ response: response.generated_text });
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao gerar resposta' });
-    }
-});
-
-app.post('/chat2', async (req, res) => {
-    const { message } = req.body;
-    return message;
-    try {
-        const response = await hf.textGeneration({
-            model: 'meta-llama/Llama-2-7b-chat-hf', // Substitua pelo modelo que você deseja usar
-            inputs: message,
-        });
-
-        res.json({ response: response.generated_text });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao gerar resposta' });
+        res.status(500).json({ 
+            error: error.message || 'An internal server error occurred', 
+            details: error 
+          });
     }
 });
 
